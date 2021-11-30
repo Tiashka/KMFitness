ff421f0679e9203d3305a9d1c701f9d8c82f7188
Tiashka <seetaltiashka@gmail.com>
2021-11-30 12:11:28 +0200

    upgrading the contact form

--- signup.php
+++ signup.php
@@ -9,33 +9,58 @@
 <body>
 <?php include("components/header.php");?>
 
- <!-- Contact Section -->
- <!-- <div class="w3-container w3-light-grey" style="padding-top:70px" id="contact">
-     <div class="w3-card-4 middle">
-        <h3 class="w3-center">CONTACT</h3>
-        <p class="w3-center w3-large">Lets get in touch. Send us a message:</p>
-        <div style="margin-top:48px">
-            <p><i class="fa fa-map-marker fa-fw w3-xxlarge w3-margin-right"></i> Chicago, US</p>
-            <p><i class="fa fa-phone fa-fw w3-xxlarge w3-margin-right"></i> Phone: +00 151515</p>
-            <p><i class="fa fa-envelope fa-fw w3-xxlarge w3-margin-right"> </i> Email: mail@mail.com</p>
-            <br>
-            <form action="/action_page.php" target="_blank">
-                <p><input class="w3-input w3-border" type="text" placeholder="Name" required name="Name"></p>
-                <p><input class="w3-input w3-border" type="text" placeholder="Email" required name="Email"></p>
-                <p><input class="w3-input w3-border" type="text" placeholder="Subject" required name="Subject"></p>
-                <p><input class="w3-input w3-border" type="text" placeholder="Message" required name="Message"></p>
-                <p>
-                    <button class="w3-button w3-black" type="submit">
-                        <i class="fa fa-paper-plane"></i> SEND MESSAGE
-                    </button>
-                </p>
-            </form>
+<header class="bgimg-mini w3-display-container w3-grayscale-min" id="home">
+        <div class="w3-display-left w3-text-white" style="padding:48px">
+            <span class="w3-jumbo w3-hide-small">KM Health and Fitness</span><br>
+            <span class="w3-xxlarge w3-hide-large w3-hide-medium">KM Health and Fitness</span><br>
+            <span class="w3-large">This form is a means for me to get to know more about you and your fitness goals. Let's get started!</span>
         </div>
+</header>
+
+
+<div class="w3-container" style="margin-top:64px;margin-bottom:64px;" id="about">
+<form class="w3-twothird w3-card-4" style="margin-left:15%;margin-right:15%">
+  <div class="w3-section formPadding" >      
+  <label>What is your specific fitness goal?</label>
+    <textarea class="w3-textarea" rows="2" required></textarea>
+  </div>
+  <div class="w3-section">      
+    <input class="w3-input" type="text" required>
+    <label>Email</label>
+  </div>
+  <div class="w3-section">      
+    <input class="w3-input" type="text" required>
+    <label>Subject</label>
+  </div>
+
+  <div class="w3-row">
+  <div class="w3-half">
+    <input id="milk" class="w3-check" type="checkbox" checked="checked">
+    <label>Milk</label>
+    <br>
+    <input id="sugar" class="w3-check" type="checkbox">
+    <label>Sugar</label>
+    <br>
+    <input id="lemon" class="w3-check" type="checkbox" disabled>
+    <label>Lemon (Disabled)</label>
+    <br><br>
+  </div>
+
+  <div class="w3-half">
+    <input id="male" class="w3-radio" type="radio" name="gender" value="male" checked>
+    <label>Male</label>
+    <br>
+    <input id="female" class="w3-radio" type="radio" name="gender" value="female">
+    <label>Female</label>
+    <br>
+    <input id="unknown" class="w3-radio" type="radio" name="gender" value="" disabled>
+    <label> Don't know (Disabled)</label>
+  </div>
+  </div>
+</form>
     </div>
-</div> -->
-<div class="w3-container w3-light-grey" style="padding-top:70px" id="contact">
-<h1> the google form goes here </h1>
-</div>
+
+
 <?php include("components/footer.php");?>
     <script src="scripts.js"></script>
 
