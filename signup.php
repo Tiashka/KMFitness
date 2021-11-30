113d186c74eaa0215255ef5e6dcb3e74f182f9f2
Tiashka <seetaltiashka@gmail.com>
2021-11-30 15:25:43 +0200

    finished the form inputs

--- signup.php
+++ signup.php
@@ -18,45 +18,110 @@
 </header>
 
 
-<div class="w3-container" style="margin-top:64px;margin-bottom:64px;" id="about">
-<form class="w3-twothird w3-card-4" style="margin-left:15%;margin-right:15%">
-  <div class="w3-section formPadding" >      
-  <label>What is your specific fitness goal?</label>
-    <textarea class="w3-textarea" rows="2" required></textarea>
+<div class="w3-container formContainer" id="about">
+<form class="w3-twothird w3-card-4" style="margin-left:15%;margin-right:15%;background-color:white;margin-top:64px;margin-bottom:64px;" method="post" action="signup.php">
+<h2 class="w3-center">Personal Information</h2>
+ 
+<div class="w3-section">      
+  <label >Name</label>
+    <input class="w3-input" type="text" name="name" required>
   </div>
+ 
   <div class="w3-section">      
-    <input class="w3-input" type="text" required>
-    <label>Email</label>
-  </div>
-  <div class="w3-section">      
-    <input class="w3-input" type="text" required>
-    <label>Subject</label>
+  <label>Email</label>
+    <input class="w3-input" type="text" name="email" required>
   </div>
 
-  <div class="w3-row">
+  <div class="w3-row w3-section">
+
+  <div class="w3-half" style="margin-left:10%;">
+      <label>What is your fitness goal?</label>
+    <br>
+        <input id="training" name="training" value="yes" class="w3-check" type="checkbox">
+        <label>Training</label>
+        <br>
+        <input id="Nutrition" name="nutrition" value="yes" class="w3-check" type="checkbox">
+        <label>Nutrition</label>
+        <br><br>
+  </div>
+</div>
+
+  <div class="w3-section" >      
+  <label>Do you have any specific fitness goal?</label>
+    <textarea class="w3-textarea" rows="2" name="specificFitness" ></textarea>
+  </div> 
+
+  <div class="w3-section">      
+  <label>Do you have any health problems / injuries currently?</label>
+    <input class="w3-input" type="text" name="healthIssues">
+  </div>
+
+
+  <div class="w3-row w3-section">
   <div class="w3-half">
-    <input id="milk" class="w3-check" type="checkbox" checked="checked">
-    <label>Milk</label>
-    <br>
-    <input id="sugar" class="w3-check" type="checkbox">
-    <label>Sugar</label>
-    <br>
-    <input id="lemon" class="w3-check" type="checkbox" disabled>
-    <label>Lemon (Disabled)</label>
+  <div style="float:left;margin-left:20%;margin-right:5%;margin-top:2%">
+      <label>Current Weight: </label>
+</div>
+<div style="float:left">
+      <input class="w3-input" style="width:40%" type="number" name="currentWeight" >
+</div>
+
+        <br><br>
+  </div>
+
+  <div class="w3-half">
+  <div style="float:left;margin-left:10%;margin-right:5%;margin-top:2%">
+    <label>Goal Weight: </label>
+    </div>
+    <div style="float:left">
+    <input class="w3-input" style="width:40%" type="number" name="goalWeight" >
+    </div>
+
     <br><br>
   </div>
+  </div>
+  <div class="w3-section" >      
+  <label>Do you have any specific outcomes you wish to achieve? (wider shoulders, slimmer waist, improved strength).</label>
+    <textarea class="w3-textarea" rows="2" name="specificFitness" ></textarea>
+  </div> 
 
-  <div class="w3-half">
-    <input id="male" class="w3-radio" type="radio" name="gender" value="male" checked>
-    <label>Male</label>
-    <br>
-    <input id="female" class="w3-radio" type="radio" name="gender" value="female">
-    <label>Female</label>
-    <br>
-    <input id="unknown" class="w3-radio" type="radio" name="gender" value="" disabled>
-    <label> Don't know (Disabled)</label>
-  </div>
-  </div>
+
+  <h2 class="w3-center">Training</h2>
+
+  <div class="w3-section" >      
+  <label>Can i get a brief overview of the equipment you have access to. (weights, gym membership, yoga mat).</label>
+    <textarea class="w3-textarea" rows="2" name="equipment" ></textarea>
+  </div> 
+
+  <h2 class="w3-center">Nutrition</h2>
+
+<div class="w3-section" >      
+<label>Are there any dietary requirements you have?</label>
+  <textarea class="w3-textarea" rows="2" name="dietaryReq" ></textarea>
+</div> 
+  <button type="submit" class="w3-button" name="submit">Submit</button>
+  
+<?php
+if(isset($_POST['submit']))
+{
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    if (isset($_POST['training'])) {
+        $training = "yes";
+      } else {
+        $training = "no";
+      }
+      if (isset($_POST['nutrition'])) {
+        $nutrition = "yes";
+      } else {
+        $nutrition = "no";
+      }
+  
+ 
+    echo "name ",$name, " email ",$email, " training ", $training," nutrition ",$nutrition;
+}
+?>
+
 </form>
     </div>
 
